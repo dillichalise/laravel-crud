d5745a35a4a6327022a11e0b8c8fac8819867af8
Dilli Chalise <dillichalise@gmail.com>
2024-03-04 16:26:25 +0900

    register method updated.

--- resources/views/user/register.blade.php
+++ resources/views/user/register.blade.php
@@ -8,7 +8,7 @@
             <p class="mb-4">Create an account to post gigs</p>
         </header>
 
-        <form action="" method="POST">
+        <form action="/user/register" method="POST">
             @csrf
             <div class="mb-6">
                 <label for="name" class="inline-block text-lg mb-2">
